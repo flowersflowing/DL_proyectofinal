809b7b1d36b788fb1a2175317c8bfeca10638c57
Natalia <nflores.teoria@gmail.com>
2021-01-11 18:31:51 -0300

    actualizando estado de los mocks

--- tests/unit/proyectos.spec.js
+++ tests/unit/proyectos.spec.js
@@ -5,3 +5,11 @@ import Vuex from 'vuex';
 import store from '../../src/store/store';
 import VueRouter from 'vue-router';
 import myRoutes from './mocks/routes';
+
+describe('Pruebas con el store', () => {
+    beforeAll(() => {
+        Vue.use(Vuex);
+        store = new Vuex.Store(store)
+    });
+});
+
